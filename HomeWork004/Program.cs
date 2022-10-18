fd1d0aa0c16688cfb4a3157e5e40a2529903dc96
RenatDavletshin <immortal1307@gmail.com>
2022-10-19 00:33:15 +0500

    FourthTask

---
+++ HomeWork004/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N.
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int count = 2;     //count число even.
+if(num < count)
+{
+    Console.WriteLine("Таких чисел нет!");
+}
+else
+    while(count <= num)
+    {
+        Console.Write($"{count}");
+        count += 2;
+    }
+Console.WriteLine();    
